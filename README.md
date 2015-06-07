903dbf01898b23e3e63083a51a0fa510b71b8ac9
kawasakiatsushi <kawasaki8910@gmail.com>
2015-06-07 22:25:09 +0900

    fix

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# capistrano
+# Capistrano Sample
+
+##環境設定
+
+* 1 Gemのインストール
+
+```
+bundle install --path vendor/bundle
+```
+
+* 2 SSH接続設定
+
+ローカルからAwsのサーバーへデプロイするための
+
+```
+
+```
--- config/deploy/{(3-1)project_name}.rb
+++ config/deploy/{(3-1)project_name}.rb
@@ -1,11 +1,11 @@
 set :stage, :production
 set :rails_env, 'production'
-server '{(3-2)IP Adress}', user: '{(3-3)user name}',
+server '{(3-1)IP Adress}', user: '{(3-2)user name}',
 roles: %w{web app db}  #何サーバーの処理を書くか。今回は同じサーバーで全部動かすのでweb app db全て指定
 
 #sshでEC２に入るのに必要
 set :ssh_options, {
-  keys: %w({(3-4)Path to pem fail}),
+  keys: %w({(3-3)Path to pem fail}),
   forward_agent: true,
   auth_methods: %w(publickey)
 }
